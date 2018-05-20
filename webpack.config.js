29e547052f9bd20061234366608771ef19b08693
Frank Panetta <frank@zuudo.com.au>
2018-05-20 12:56:40 +1000

    correctly test env var

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = function(env = 'dev') {
   const config = require(`./webpack/webpack.${env}.js`);
-  config.mode = (env === 'production' ? 'production' : 'development');
+  config.mode = (env === 'prod' ? 'production' : 'development');
   return config;
 };
